b43da72fd61f31a61f148916ccb911d81f1ed061
Alex Frd <chugun777akk@mail.ru>
2026-04-03 10:38:24 +0700

    add login page frontend

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
@@ -10,9 +10,8 @@ class IndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = "Тестовый сервер работает"
+        context["title"] = "Геккон тестирование"
         return context
 
 
-def index(request):
-    return HttpResponse("Home page sjbsjkaskvkhagscvck")
+
